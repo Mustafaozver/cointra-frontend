11b96765d3beed7ca2aeabd2b1cfa427003dcb2d
mustafaozver <mustafa.ozver@hotmail.com>
2022-09-24 21:29:08 +0300

    24.09.2022-5 Commit - Schema changed for home page

--- src/pages/properties/propertiesListPage/PropertiesListPage.jsx
+++ src/pages/properties/propertiesListPage/PropertiesListPage.jsx
@@ -18,6 +18,30 @@ import ContactPopup from '../../../components/popup/contactPopup/ContactPopup';
 import PropertiesFilters from '../../../components/propertiesFilters/PropertiesFilters';
 import NavigationBar from '../../../components/NavigationBar/NavigationBar';
 
+const schemaJson = {
+  '@context': 'https://schema.org',
+  '@type': 'Organization',
+  name: 'Zeekeez',
+  url: 'https://www.zeekeez.com',
+  logo: 'https://www.zeekeez.com/images/zeekeez-logo-black.svg',
+  description: 'Zeekez.com is the 1st UAE’s Property Portal for people to explore, research and share their interests for the UAE property market.',
+  telephone: '+97145781335',
+  foundingDate: '2022',
+  contactPoint: {
+    '@type': 'ContactPoint',
+    telephone: '+97145781335',
+    contactType: 'customer service',
+    areaServed: 'AE',
+    availableLanguage: ['en', 'ar']
+  },
+  sameAs: [
+    'https://www.facebook.com/people/Zeekeez/100079499966747/',
+    'https://www.instagram.com/zeekeez_official/',
+    'https://www.linkedin.com/company/zeekeez',
+    'https://www.zeekeez.com/'
+  ]
+};
+
 const PropertiesListPage = (props) => {
   const {
     initialFilters,
@@ -161,6 +185,7 @@ const PropertiesListPage = (props) => {
 
   return (
     <div className={styles['properties-list-page']}>
+      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaJson) }} />
       <MyPZContainer>
         <ContactPopup
           isOpen={isContactOpen}
